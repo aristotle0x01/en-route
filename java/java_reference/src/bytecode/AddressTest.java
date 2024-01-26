ee17fbf0f3f6c4d7cce225e8d0b51f5c9921f0f7
aristotle0x01 <aristotle0x01@protonmail.com>
2024-01-26 15:21:29 +0800

    minor

--- java/java_reference/src/bytecode/AddressTest.java
+++ java/java_reference/src/bytecode/AddressTest.java
@@ -5,10 +5,19 @@ package bytecode;
 //-Xint:methodName：针对特定方法启用纯解释执行模式。可以指定要在解释执行模式下执行的特定方法的名称。
 
 // -Xint -XX:-UseCompressedOops -XX:+UnlockDiagnosticVMOptions -XX:+PrintInterpreter
+// -Xint -XX:-UseCompressedOops -XX:+UnlockDiagnosticVMOptions -XX:-DebugNonSafepoints -XX:CompileCommand=print,java/lang/Object.hashCode
+// -Xcomp -XX:-UseCompressedOops -XX:+UnlockDiagnosticVMOptions -XX:+PrintAssembly -XX:-DebugNonSafepoints
+// -XX:-UseCompressedOops -XX:+UnlockDiagnosticVMOptions -XX:-DebugNonSafepoints -XX:CompileCommand="print java.lang.Object::toString" AddressTest
+// ./java -Xcomp -server -XX:+UnlockDiagnosticVMOptions '-XX:CompileCommand=print,AddressTest::test' -XX:-DebugNonSafepoints AddressTest
+
 public class AddressTest {
     public static void main(String args[]) throws Exception {
-        int i = 1;
+        int i = AddressTest.test(1);
         System.out.println(i);
+    }
+
+    public static int test(int i){
+        return i+101;
     }
 }
 
