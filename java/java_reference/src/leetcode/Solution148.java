42682cd1c5b3ce7004d16c7cbb8cde3d2e1fe7a6
aristotle0x01 <aristotle0x01@protonmail.com>
2024-03-11 10:29:41 +0800

    leetcode 148

--- java/java_reference/src/leetcode/Solution148.java
+++ java/java_reference/src/leetcode/Solution148.java
@@ -1,5 +1,8 @@
 package leetcode;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Solution148 {
 
     // 0148
@@ -13,8 +16,77 @@ public class Solution148 {
         ListNode(int val, ListNode next) { this.val = val; this.next = next; }
     }
 
-    // [4,2,1,3]
+    // using the concept of merge sort
     public ListNode sortList(ListNode head) {
+        if (head == null || head.next == null) {
+            return head;
+        }
+
+        List<ListNode> list = divide(head);
+        ListNode h1 = sortList(list.get(0));
+        ListNode h2 = sortList(list.get(1));
+        return merge(h1, h2);
+    }
+
+    // using fast and slow pointer
+    private List<ListNode> divide(ListNode h){
+        ListNode c = h;
+        ListNode n = h.next.next;
+        // [4,2,1,3]
+        while (n != null) {
+            c = c.next;
+            n = n.next;
+            if (n != null){
+                n = n.next;
+            }
+        }
+
+        ListNode h2 = c.next;
+        c.next = null;
+        ArrayList<ListNode> list = new ArrayList<>(2);
+        list.add(h);
+        list.add(h2);
+        return list;
+    }
+
+    private ListNode merge(ListNode h1, ListNode h2){
+        ListNode h = null;
+        ListNode cur = null;
+        while (h1 != null && h2 != null) {
+            if (h1.val < h2.val){
+                if (h == null) {
+                    h = h1;
+                    cur = h;
+                } else {
+                    cur.next = h1;
+                    cur = h1;
+                }
+                h1 = h1.next;
+            } else {
+                if (h == null) {
+                    h = h2;
+                    cur = h;
+                } else {
+                    cur.next = h2;
+                    cur = h2;
+                }
+                h2 = h2.next;
+            }
+        }
+        while (h1 != null) {
+            cur.next = h1;
+            break;
+        }
+        while (h2 != null) {
+            cur.next = h2;
+            break;
+        }
+
+        return h;
+    }
+
+    // [4,2,1,3]
+    public ListNode sortList1(ListNode head) {
         ListNode h = head;
         ListNode nex = (h != null) ? h.next : null;
         ListNode cur = head;
