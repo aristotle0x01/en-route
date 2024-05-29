698498c5d8711a83554181b5513e74e08e98cbce
aristotle0x01 <aristotle0x01@protonmail.com>
2024-05-29 14:22:48 +0800

    leetcode 1140 refine

--- java/java_reference/src/leetcode/StoneGameII1140.java
+++ java/java_reference/src/leetcode/StoneGameII1140.java
@@ -1,14 +1,12 @@
 package leetcode;
 
-import java.util.HashMap;
-
 public class StoneGameII1140 {
     // https://leetcode.com/problems/stone-game-ii/description/
 
     private int n;
     private int[] piles;
     private int[] sums;
-    private HashMap<String, Integer> map;
+    private Integer[][][] cache;
 
     public int stoneGameII(int[] piles) {
         this.n = piles.length;
@@ -21,7 +19,7 @@ public class StoneGameII1140 {
             return sum;
         }
 
-        map = new HashMap<>();
+        cache = new Integer[n][2*n][2*n];
         sums = new int[n];
         sums[n-1] = piles[n-1];
         for (int j=n-2; j>=0; j--) {
@@ -36,19 +34,13 @@ public class StoneGameII1140 {
         return max;
     }
 
-    private String key(int i, int x, int M) {
-        return String.format("%d-%d-%d", i, x, M);
-    }
-
     private int dfs(int i, int x, int M) {
-        String key = key(i, x, M);
-        if (map.containsKey(key)) {
-            return map.get(key);
-        }
-
         int acc = 0;
         if (i >= n) {
             return acc;
+        }
+        if (cache[i][x][M] != null) {
+            return cache[i][x][M];
         }
 
         int loop = 0;
@@ -59,7 +51,7 @@ public class StoneGameII1140 {
             j++;
         }
         if (j == n) {
-            map.put(key, acc);
+            cache[i][x][M] = acc;
             return acc;
         }
 
@@ -70,14 +62,13 @@ public class StoneGameII1140 {
             max = Math.max(dfs(j, k, MBob), max);
         }
 
-        int value = acc + (sums[j] - max);
-        map.put(key, value);
-        return value;
+        cache[i][x][M] = acc + (sums[j] - max);
+        return cache[i][x][M];
     }
 
     public static void main(String[] args) {
         StoneGameII1140 s = new StoneGameII1140();
-        // int[] arr = new int[]{2,7,9,4,4};
+        // int[] arr = new int[]{1,1,1};
         int[] arr = new int[]{
                 7678,8845,4908,113,5895,9508,6793,1075,1520,800,8935,7774,571,2042,9164,9886,2625,6733,7565,2940,1517,8138,1408,4936,2406,7851,2555,6883,2032,4572,7577,6060,870,1848,5442,8578,2161,6175,6069,5153,8786,4679,4320,3414,1180,4790,208,2269,7261,8187,4014,6908,7845,673,370,9753,4241,4871,25
         };
